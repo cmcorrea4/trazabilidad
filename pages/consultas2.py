4ecddc144ecc95fda6a9ea3b89763b081b7a252e
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-04-20 11:43:44 -0500

    Update consultas2.py

--- pages/consultas2.py
+++ pages/consultas2.py
@@ -7,6 +7,11 @@ import pandas as pd
 from datetime import datetime
 
 
+time_c=1
+token = "VmIHuN_GB8AhmOchqnjtgrOL-oD2pHU-2ypKcswWbtM6aY1G2ylRYOJQpsqEANVl9iZ5PdAGqTsOJ30NPCtPUQ=="
+org = "cmcorrea4@gmail.com"
+bucket = "Elec_var"
+client_Inf = InfluxDBClient(url="https://eu-central-1-1.aws.cloud2.influxdata.com", token=token,org=org,verify_ssl=False)
 
 fields = ["Orden", "Proceso", "Estado"]
 data = {field: [] for field in fields}
@@ -20,6 +25,11 @@ for field in fields:
             time_data[field].append(record.get_time())
             data[field].append(record.get_value())
 
-df_temp = pd.DataFrame(data["Orden"], columns=["Orden"])
-df_hum = pd.DataFrame(data["Proceso"], columns=["Proceso"])
-df_senst = pd.DataFrame(data["Estado"], columns=["Estado"])
+df_Orden = pd.DataFrame(data["Orden"], columns=["Orden"])
+df_Proceso = pd.DataFrame(data["Proceso"], columns=["Proceso"])
+df_Estado = pd.DataFrame(data["Estado"], columns=["Estado"])
+
+df_consulta = pd.concat([df_Orden, df_Proceso, df_Estado], axis=1)
+st.dataframe(df_consulta)
+
+
