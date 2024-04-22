9fdee4dd199ec0506d7c114c5b557083e4149b82
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-04-22 12:25:22 -0500

    Update consultas.py

--- pages/consultas.py
+++ pages/consultas.py
@@ -21,7 +21,9 @@ fields = ["Orden", "Proceso", "Estado"]
 data = {field: [] for field in fields}
 time_data = {field: [] for field in fields}
 
-for field in fields:
+try:
+
+ for field in fields:
     query = f'from(bucket: "{bucket}")|> range(start: -{time_c}h)|> filter(fn: (r) => r._field == "{field}" )'
     tables = client_Inf.query_api().query(query, org)
     for table in tables:
@@ -29,30 +31,31 @@ for field in fields:
             time_data[field].append(record.get_time())
             data[field].append(record.get_value())
 
-serie_time = pd.Series(time_data[field])
-serie_tim=pd.DatetimeIndex(pd.to_datetime(serie_time,unit='s')).tz_convert('America/Bogota')   #tz_convert('America/Bogota')
-index_time=serie_tim
-index_time_s=index_time.strftime('%Y-%m-%d %H:%M:%S')
+ serie_time = pd.Series(time_data[field])
+ serie_tim=pd.DatetimeIndex(pd.to_datetime(serie_time,unit='s')).tz_convert('America/Bogota')   #tz_convert('America/Bogota')
+ index_time=serie_tim
+ index_time_s=index_time.strftime('%Y-%m-%d %H:%M:%S')
 
-df_Orden = pd.DataFrame(data["Orden"], columns=["Orden"])
-df_Proceso = pd.DataFrame(data["Proceso"], columns=["Proceso"])
-df_Estado = pd.DataFrame(data["Estado"], columns=["Estado"])
-df_time_data = pd.DataFrame(index_time_s, columns=["Time_data"])
+ df_Orden = pd.DataFrame(data["Orden"], columns=["Orden"])
+ df_Proceso = pd.DataFrame(data["Proceso"], columns=["Proceso"])
+ df_Estado = pd.DataFrame(data["Estado"], columns=["Estado"])
+ df_time_data = pd.DataFrame(index_time_s, columns=["Time_data"])
 
 
-df_consulta = pd.concat([df_Orden, df_Proceso, df_Estado, df_time_data], axis=1)
-st.dataframe(df_consulta)
+ df_consulta = pd.concat([df_Orden, df_Proceso, df_Estado, df_time_data], axis=1)
+ st.dataframe(df_consulta)
 
-orden_seleccionado = st.selectbox('Selecciona una Orden:', df_consulta['Orden'].unique())
+ orden_seleccionado = st.selectbox('Selecciona una Orden:', df_consulta['Orden'].unique())
 
 # Filtrar el DataFrame por el estado seleccionado
-filtered_df = df_consulta[df_consulta['Orden'] == orden_seleccionado]
+ filtered_df = df_consulta[df_consulta['Orden'] == orden_seleccionado]
 
-# Mostrar el DataFrame resultante cuando se presiona el botón
-if st.button('Consultar'):
-    st.write("DataFrame filtrado:")
-    st.write(filtered_df)
-
+ # Mostrar el DataFrame resultante cuando se presiona el botón
+ if st.button('Consultar'):
+     st.write("DataFrame filtrado:")
+     st.write(filtered_df)
+except:
+    pass
 
 
 
